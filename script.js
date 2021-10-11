391ffb8d89ecb50e89dd3c56cf47dc1b4c2e874a
franco <nahuelgodina@gmail.com>
2021-10-11 13:44:39 -0300

    Finished

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ defaultGrid();
 
 function pickCols () {
     let howmanyCols = prompt("How many columns would you like?(max:100)");
-
-    return howmanyCols;
+    let numofCols = parseInt(howmanyCols);
+    return numofCols;
 }
 
 const clearGrid = document.querySelector("#clearGrid");
@@ -23,7 +23,14 @@ const resizeGrid = document.querySelector("#resizeGrid");
 resizeGrid.addEventListener("click", () => {
     newGrid();
 })
-
+const black = document.querySelector("#black");
+black.addEventListener("click", () => {
+    colorBlack();
+})
+const rainbow = document.querySelector("#rainbow");
+rainbow.addEventListener("click", () => {
+    colorRainbow();
+})
 
 function clearContainer() {
     container.innerText = "";
@@ -45,6 +52,10 @@ function newGrid () {
         alert ("It's not allowed to go beyond 100!")
         newGrid();
     }
+    else if (isNaN(cols)) {
+        alert ("That's not a number")
+        newGrid();
+    }
     else {
         container.style.cssText = "display: grid; grid-template-columns: repeat("+cols+", 1fr); grid-template-rows: repeat("+cols+", 1fr); height: 600px; width: 600px; border: 5px solid black; margin: auto;";
         clearContainer();
@@ -58,12 +69,33 @@ function makeGrid(cols) {
         const box = document.createElement("div");
         container.appendChild(box);
         box.addEventListener("mouseover", function(event) {
-            event.target.style.backgroundColor = "black";
-        
+            if (currentColor === "rainbow") {
+                const randomR = Math.floor(Math.random() * 256);
+                const randomG = Math.floor(Math.random() * 256);
+                const randomB = Math.floor(Math.random() * 256);
+                event.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`
+            }
+            else if (currentColor === "black") {
+                event.target.style.backgroundColor = "black";
+            }
         }, false);
     }
 }
 
+let currentColor = "black";
+
+function colorBlack() {
+    currentColor = "black";
+}
+function colorRainbow() {
+    currentColor = "rainbow";
+}
+
+
 clearGrid.style.cssText = "background-color: red; border-radius: 12px; border: 2px red; color: white; padding: 16px 40px; font-size: 24px; margin: 10px; text-align: center; font-family: 'Orbitron', sans-serif;";
 
 resizeGrid.style.cssText = "background-color: #4CAF50; border-radius: 12px; border: 2px #4CAF50; color: white; padding: 16px 40px; font-size: 24px; margin: 6px; text-align: center; font-family: 'Orbitron', sans-serif;";
+
+black.style.cssText = "background-color: black; border-radius: 12px; border: 2px #4CAF50; color: white; padding: 16px 40px; font-size: 24px; margin: 6px; text-align: center; font-color: white; font-family: 'Orbitron', sans-serif;";
+
+rainbow.style.cssText = "background: linear-gradient(45deg, purple, lightblue, yellow, green); border-radius: 12px; border: 2px #4CAF50; color: white; padding: 16px 40px; font-size: 24px; margin: 6px; text-align: center; font-family: 'Orbitron', sans-serif;";
